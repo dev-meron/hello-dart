8f67959ba2ee3e803f574f3e6fe79e5504df9214
dev-meron <merijourney97@gmail.com>
2025-12-02 14:29:52 -0600

    Add main function and print UserInfo function

---
+++ function.dart
@@ -0,0 +1,10 @@
+void main() {
+  // Call the function with sample data
+  printUserInfo("Alice", 25);
+}
+
+// Function definition
+void printUserInfo(String name, int age) {
+  print("User Name: $name");
+  print("User Age: $age");
+}
